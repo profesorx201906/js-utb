3bea4aa9263f33a5790008f3bc26710f06182eff
Javier Díaz Díaz <jddiazsena@gmail.com>
2025-05-07 09:33:34 -0500

    ciclos While y Do While

---
+++ 07. Mayo 07/mientras.js
@@ -0,0 +1,38 @@
+// ---- Ejemplo básico
+/*
+let numero = 0;
+
+while (numero < 90) {
+  console.log(numero);
+  numero=parseInt(Math.random()*100);  
+}
+console.log("Finalizó ciclo While");*/
+
+// ---- Ejemplo recorrer cadena hasta caracter ,
+
+/*
+let cadena = "Este es un ejemplo de cadena, para el ciclo while";
+let i = 0;
+while (cadena[i] !== ",") {
+  console.log(cadena[i]);
+  i++;
+}
+*/
+
+// ---- Ejemplo do while
+
+let n = 0;
+console.log("Inicio Ciclo while");
+while (n !== 0) {
+  console.log("el valor de n =" + n);
+  n=0;
+}
+console.log("Fin Ciclo while");
+console.log("Inicio Ciclo  do while");
+do {
+  console.log("el valor de n =" + n);
+  n=0;
+} while (n !== 0);
+console.log("Fin Ciclo do while");
+
+
