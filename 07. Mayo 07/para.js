cf249f67b77e2c8d828a91f434a3843da606165f
Javier Díaz Díaz <jddiazsena@gmail.com>
2025-05-07 07:54:48 -0500

    para imprimir numeros pares

--- 07. Mayo 07/para.js
+++ 07. Mayo 07/para.js
@@ -1,7 +1,14 @@
+/* ejemplo básico
 let n = 0;
 for (let i = 0; i < 9; i++) {
   n += i;
   //n=n+i;
 }
 console.log(n);
+*/
 
+//hacer un ciclo que imprima los numeros pares desde el numero 10 hasta el numero 30 inclusives
+
+for (let i = 10; i <= 30; i+=2) {
+  console.log(i);
+}
