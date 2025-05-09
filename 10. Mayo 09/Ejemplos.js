4a47541f432a8de3c8dd85ca2fd90fd73d9bbe05
Javier Díaz Díaz <jddiazsena@gmail.com>
2025-05-09 07:29:48 -0500

    Ejemplo declaracion tipos primitivos y estructurados

---
+++ 10. Mayo 09/Ejemplos.js
@@ -0,0 +1,25 @@
+// ejemplo declaracion variable tipo de dato primitivo permite cambiar valores
+
+let Equipo = "Nacional";
+Equipo = "Junior";
+console.log(Equipo);
+
+// ejemplo declaracion variable tipo de dato estructurado permite cambiar valores
+
+let Frutas = ["manzana","pera"];
+Frutas.push("Banana")
+console.log(Frutas);
+
+
+// ejemplo declaracion constante tipo de dato estructurado permite cambiar valores
+
+const edades = [52,36,39];
+edades.push(12);
+console.log(edades);
+
+
+// ejemplo declaracion constante tipo de dato primitivo permite cambiar valores
+
+const nombre = "Hever";
+nombre = "Hever Torres Delanoys";
+
