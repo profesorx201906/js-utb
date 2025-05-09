3ce659daed4472b4d23e2d0e7413af7c143a1770
Javier Díaz Díaz <jddiazsena@gmail.com>
2025-05-09 07:44:31 -0500

    ejemplo recorrer arreglo metodo propio forEach

--- 10. Mayo 09/Ejemplos.js
+++ 10. Mayo 09/Ejemplos.js
@@ -19,7 +19,24 @@ console.log(edades);
 
 
 // ejemplo declaracion constante tipo de dato primitivo permite cambiar valores
-
+/*
 const nombre = "Hever";
-nombre = "Hever Torres Delanoys";
+nombre = "Hever Torres Delanoys";*/
 
+//ejemplo llenado de vector
+
+const estratos = [];
+let n=0;
+
+console.log(estratos);
+while(n<10){
+  estratos.push(parseInt(Math.random()*10))
+  n++;
+}
+console.log(estratos);
+
+// recorrer un arreglo con su propio metodo sin un ciclo externo
+
+estratos.forEach(function(estrato, indice){
+  console.log(estrato + " " + indice + " desde forEach");  
+})
