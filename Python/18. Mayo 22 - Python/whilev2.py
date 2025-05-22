06b90ebd7896076d6f3c6cd387d33a3c2d33e205
Javier Díaz Díaz <jddiazsena@gmail.com>
2025-05-22 07:59:36 -0500

    Ejemplo while v2

--- Python/18. Mayo 22 - Python/whilev2.py
+++ Python/18. Mayo 22 - Python/whilev2.py
@@ -1,20 +1,23 @@
 # saber la cantidad de compras (solo tener en cuenta las compras con valores positivos) y el promedio de las compras
 # Entrada
-monto = -1
 totalCompras = 0
 cantidad = 0
 promedio = 0
 # Proceso
-while monto != 0:
+while True:
     monto = float(input("Digite el valor de la compra : "))
+    #---
+    if monto == 0:
+        break
+    #---
     if monto < 0:
         print("no se puede sumar el valor de la compra por ser menor que 0")
     else:
         totalCompras += monto
-        if monto!=0:
-          cantidad += 1
+        cantidad += 1
 # Salida
-promedio = totalCompras / cantidad
+if cantidad>0:
+  promedio = totalCompras / cantidad
 print(
     f"se realizaron {cantidad} de compras. \nPromedio fue de {promedio} y el total de las compras es {totalCompras:,.2f} "
 )
