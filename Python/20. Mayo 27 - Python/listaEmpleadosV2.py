c6541b04b7574cbe7bdd1a51157d689ec690708e
Javier Díaz Díaz <jddiazsena@gmail.com>
2025-05-28 07:56:06 -0500

    Ejemp empleados uso faker

--- Python/20. Mayo 27 - Python/listaEmpleadosV2.py
+++ Python/20. Mayo 27 - Python/listaEmpleadosV2.py
@@ -1,8 +1,62 @@
 # Elaborar un algoritmo que permita almacenar información un grupo de empleados para los cuales se debe solicitar la siguiente información  Identificación , Nombre, Apellido, Edad, Salario
 # debemos imprimir el salario promedio de los empleados, debemo imprimir el empleado con mayor salario y debemos imprimir todos los empleados que tengan el apellido Berrocal
 import os
+from faker import Faker
+from faker.providers import BaseProvider
+import random
+
+fake = Faker()
 
 empleados = []
+Faker.seed(0)
+
+
+
+class nombreEmpleado(BaseProvider):
+    def nombre_emp(self):
+        items = ["Maria Feranda", "Oscar Burgos", "Hever Torres", "Adrian Yanes"]
+        return self.random_element(items)
+
+
+class CustomSalaryProvider(BaseProvider):
+    def custom_salary(self):
+        return self.random_element([x for x in range(1500000, 5000001, 100000)])
+
+
+class PersonalItemProvider(BaseProvider):
+    def personal_item(self):
+        items = [
+            "sombrero",
+            "sombrilla",
+            "gafas",
+            "bolso",
+            "reloj",
+            "bufanda",
+            "chaqueta",
+            "mochila",
+            "guantes",
+            "cartera",
+            "paraguas",
+            "celular",
+            "libro",
+            "audífonos",
+            "botella de agua",
+        ]
+        return self.random_element(items)
+
+
+fake.add_provider(CustomSalaryProvider)
+fake.add_provider(PersonalItemProvider)
+fake.add_provider(nombreEmpleado)
+for i in range(10):
+    empleado = {}
+    empleado["indentificacion"] = fake.random_number(digits=10, fix_len=True)
+    nombre = fake.nombre_emp().split(" ")
+    empleado["nombre"] = nombre[0]
+    empleado["apellido"] = nombre[1]
+    empleado["edad"] = random.randint(18, 60)
+    empleado["salario"] = fake.custom_salary()
+    empleados.append(empleado)
 
 
 def limpiar_pantalla():
@@ -10,6 +64,19 @@ def limpiar_pantalla():
         os.system("cls")
     else:
         os.system("clear")
+
+
+def leerDatoNumerico(mensaje, tipo):
+    while True:
+        try:
+            if tipo == "int":
+                valor = int(input(mensaje))
+            elif tipo == "float":
+                valor = float(input(mensaje))
+            return valor
+        except ValueError:
+            print("Debe digitar un dato numérico, presione tecla para continuar")
+            input()
 
 
 def menu():
@@ -23,16 +90,16 @@ def menu():
 
 
 def imprimirEmpleados(empleados):
-    print("-" * 80)
+    print("-" * 60)
     print(
         f"{'Identificacion':<10} {'Nombre':<10} {'Apellido':<10}{'Edad':>5}{'Salario':>10}"
     )
-    print("-" * 80)
+    print("-" * 60)
     for emp in empleados:
         print(
             f"{emp['indentificacion']:<15}{emp['nombre']:<10}{emp['apellido']:<10}{emp['edad']:>5}{emp['salario']:>15,.2f}"
         )
-    print("-" * 80)
+    print("-" * 60)
 
 
 def llenarDatos(empleados):
@@ -40,8 +107,10 @@ def llenarDatos(empleados):
     empleado["indentificacion"] = input(f"Digite la identificacion del empleado : ")
     empleado["nombre"] = input(f"Digite la nombre del empleado : ")
     empleado["apellido"] = input(f"Digite la apellido del empleado : ")
-    empleado["edad"] = int(input(f"Digite la edad del empleado : "))
-    empleado["salario"] = float(input(f"Digite la salario del empleado : "))
+    empleado["edad"] = leerDatoNumerico(f"Digite la edad del empleado : ", "int")
+    empleado["salario"] = leerDatoNumerico(
+        f"Digite el salario del empleado : ", "float"
+    )
     empleados.append(empleado)
 
 
