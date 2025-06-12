774942f1e5d1410817f34ba282fd8ae82494465e
profesorx <jddiaz@misena.edu.co>
2025-06-12 09:38:39 -0500

    Limpieza Ortográfica - Corregir errores ortografía

--- Python/23. Junio 12 - Python Limpieza/limpieza ortografica.py
+++ Python/23. Junio 12 - Python Limpieza/limpieza ortografica.py
@@ -14,21 +14,30 @@ print("Limpieza Ortográfica - Data original")
 print(df)
 
 print("convertir a tipo título (inicial en mayúscula) las columnas nombre y ciudad")
-df["Nombre"]=df["Nombre"].str.title()
-df["Ciudad"]=df["Ciudad"].str.title()
+df["Nombre"] = df["Nombre"].str.title()
+df["Ciudad"] = df["Ciudad"].str.title()
 print(df)
 
-
+print("Presione tecla para continuar")
+input()
 lp.limpiar_pantalla()
+print("Data original")
+print(df)
+# Agrupar
+print("AGrupar por ciudad")
+df_grupo_ciudad = df.groupby("Ciudad").ValorFactura.sum().reset_index()
+print(df_grupo_ciudad)
 
-# Cargar la data
-df = pd.read_excel(f"{ruta}\LimpiezaOrtografia.xlsx", sheet_name="Datos")
-print("Limpieza Ortográfica - Data original")
+print("Presione tecla para continuar")
+input()
+lp.limpiar_pantalla()
+print("Data original")
+print(df)
+# Agrupar
+print("corregir nombres")
+df.replace({"Ciudad": {"Bogota": "Bogotá", "Medellin": "Medellín"}}, inplace=True)
 print(df)
 
-print("convertir a tipo título (inicial en mayúscula) las columnas nombre y ciudad")
-df["Nombre"]=df["Nombre"].str.title()
-df["Ciudad"]=df["Ciudad"].str.title()
-print(df)
-
-
+print("AGrupar por ciudad")
+df_grupo_ciudad = df.groupby("Ciudad").ValorFactura.sum().reset_index()
+print(df_grupo_ciudad)
